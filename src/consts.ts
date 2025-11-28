f1c2d124e0d81c23fc3fe2cc69044ff498f08508
azreenbd <nasxgeek@gmail.com>
2025-11-29 05:04:33 +0800

    update styling

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Naszrul Azreen';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'A Full Stack Web Developer with over 4 years of experience in web development.';

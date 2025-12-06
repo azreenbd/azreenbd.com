64d0373b7fb06d49387d3148eda43468982cc36e
azreenbd <nasxgeek@gmail.com>
2025-12-06 17:23:34 +0800

    style revamp, add project

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,5 +15,20 @@ const blog = defineCollection({
 			heroImage: image().optional(),
 		}),
 });
+const project = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/project', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			stacks: z.array(z.string()).optional(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+		}),
+});
 
-export const collections = { blog };
+export const collections = { blog, project };
